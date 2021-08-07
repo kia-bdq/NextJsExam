9b2ab67decd6f1cd6c1858d8e2962ff69820a17e
Kiandokht <Kiandokht@DESKTOP-5MVAQTA>
2021-08-07 18:10:24 +0430

    axios and formik was edited

--- src/pages/axios.tsx
+++ src/pages/axios.tsx
@@ -1,18 +1,35 @@
 import axios from "axios";
-import IUser from "../types/user";
+import { IUser } from "../types/user";
 // you don't need anything else to import
 
 interface Props {
   // TODO: add user type
+  users : IUser[];
 }
+const instance = axios.create({
+  baseURL: 'http://localhost:3000/api/',
+});
 
-export default function AxiosTest(props: Props) {
+export default function AxiosTest({ props }: Props) {
   // TODO: once you get data map through data and show them name
-  return <div>Simple Axios And Data Fetching :: Edit src/pages/axios.tsx</div>;
+  return <div>
+          {props.users.map((user : IUser) => (<p key={user.name}>{user.name}</p>))}
+        </div>;
 }
 
 // TODO: fetch data with axios to use in app
 // RestApi: axios.get("/api/users") => [{name: "amir"}]
-// NOTE: data fetching should happen in server side
+// NOTE: data fetching should happen in server side 
 // Resource: https://nextjs.org/docs/basic-features/data-fetching
 // Resource: https://github.com/axios/axios
+
+export async function getServerSideProps() {
+  try {
+    const res = await instance.get('/users');
+    const props : Props = { users: res.data }
+    return { props: { props } }
+  } 
+  catch (error) {
+  }
+}
+
--- src/pages/formik.tsx
+++ src/pages/formik.tsx
@@ -1,4 +1,5 @@
 import { Input, Form, Button, Space } from "antd";
+import { useFormik } from 'formik';
 
 const FormItem = Form.Item;
 
@@ -12,24 +13,62 @@ export default function FormikTest() {
   // BONUS: adding validation has extra points
   // Resource: https://formik.org/docs/tutorial
 
-  const handleSubmit = (values: IForm) => {
-    console.log(values);
+  const validate = values => {
+
+    const errors = {};
+
+    if (!values.username) {
+      errors.username = 'Required';
+    } else if (values.username.length < 3) {
+      errors.username = 'Must be 3 characters or more';
+    }
+ 
+    if (!values.password) {
+      errors.password = 'Required';
+    } else if (values.password.length <3 ) {
+      errors.password = 'Must be 3 characters or more';
+    }
+    return errors;
   };
+  
+  const formik = useFormik({
+
+    initialValues: {
+
+      username: '',
+      password: ''
+
+    },
+    validate,
+
+    onSubmit: (values: IForm) => {
+      console.log(values);
+    },
+
+  });
 
   return (
     <div>
       <div>Simple Formik With Antd Inputs :: Edit src/pages/formik.tsx</div>
       <hr />
       {/* TODO: use Formik */}
-      <form>
+      <form onSubmit={formik.handleSubmit}>
         {/* TODO: make this inputs work with formik i suggest make it a reusable component like FormikInput */}
-        <FormItem label="Username" name="username">
-          <Input />
+        
+        <FormItem label="Username" >
+          <Input name="username" onChange={formik.handleChange} value={formik.values.username}/>
+          {formik.errors.username ? <div>{formik.errors.username}</div> : null}
         </FormItem>
+        
         {/* TODO: make this inputs work with formik i suggest make it a reusable component like FormikInput */}
-        <FormItem label="Password" name="password">
-          <Input />
+        
+        <FormItem label="Password" >
+          <Input name="password" onChange={formik.handleChange} value={formik.values.password} />
+          {formik.errors.password ? <div>{formik.errors.password}</div> : null}
         </FormItem>
+        
+        
+        
         <Space>
           <Button type="primary" htmlType="submit">
             submit
@@ -37,6 +76,7 @@ export default function FormikTest() {
           <Button
             onClick={() => {
               // TODO: reset form with formik
+              
             }}
           >
             reset
